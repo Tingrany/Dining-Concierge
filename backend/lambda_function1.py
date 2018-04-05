143725bda7ade36b02b50fda628f1cd505bb1804
Yang Hu <yh2948@columbia.edu>
2018-04-04 22:06:58 -0400

    Remove close code from lambda1

--- backend/lambda_function1.py
+++ backend/lambda_function1.py
@@ -31,18 +31,6 @@ def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message)
             'message': message
         }
     }
-    
-
-def close(session_attributes, fulfillment_state, message):
-    response = {
-        'sessionAttributes': session_attributes,
-        'dialogAction': {
-            'type': 'Close',
-            'fulfillmentState': fulfillment_state,
-            'message': message
-        }
-    }
-    return response
 
 
 def delegate(session_attributes, slots):
